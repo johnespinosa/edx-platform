5af28e063c5f901a696cd9c9db173f1995da15d0
Calen Pennington <cale@edx.org>
2015-03-27 14:05:44 -0400

    Extract a pure-XBlock version of MakoModuleDescriptor

--- common/lib/xmodule/xmodule/mako_module.py
+++ common/lib/xmodule/xmodule/mako_module.py
@@ -1,4 +1,8 @@
-from .x_module import XModuleDescriptor, DescriptorSystem
+"""
+Code to handle mako templating for XModules and XBlocks.
+"""
+from .x_module import XModuleDescriptor, DescriptorSystem, shim_xmodule_js
+from xblock.fragment import Fragment
 
 
 class MakoDescriptorSystem(DescriptorSystem):
@@ -8,7 +12,7 @@ class MakoDescriptorSystem(DescriptorSystem):
         self.render_template = render_template
 
 
-class MakoModuleDescriptor(XModuleDescriptor):
+class MakoTemplateBlock(object):
     """
     Module descriptor intended as a mixin that uses a mako template
     to specify the module html.
@@ -17,11 +21,11 @@ class MakoModuleDescriptor(XModuleDescriptor):
     with the name of the template to render, and it will pass
     the descriptor as the `module` parameter to that template
 
-    MakoModuleDescriptor.__init__ takes the same arguments as xmodule.x_module:XModuleDescriptor.__init__
+    MakoTemplateBlock.__init__ takes the same arguments as xmodule.x_module:XModuleDescriptor.__init__
     """
 
     def __init__(self, *args, **kwargs):
-        super(MakoModuleDescriptor, self).__init__(*args, **kwargs)
+        super(MakoTemplateBlock, self).__init__(*args, **kwargs)
         if getattr(self.runtime, 'render_template', None) is None:
             raise TypeError(
                 '{runtime} must have a render_template function'
@@ -39,6 +43,30 @@ class MakoModuleDescriptor(XModuleDescriptor):
             'editable_metadata_fields': self.editable_metadata_fields
         }
 
+    def studio_view(self, context):  # pylint: disable=unused-argument
+        """
+        View used in Studio.
+        """
+        # pylint: disable=no-member
+        fragment = Fragment(
+            self.system.render_template(self.mako_template, self.get_context())
+        )
+        shim_xmodule_js(self, fragment)
+        return fragment
+
+
+class MakoModuleDescriptor(MakoTemplateBlock, XModuleDescriptor):
+    """
+    Mixin to use for XModule descriptors.
+    """
     def get_html(self):
-        return self.system.render_template(
-            self.mako_template, self.get_context())
+        return self.studio_view(None).content
+
+    # Present only to appease pylint.
+    def export_to_xml(self, resource_fs):
+        return super(MakoModuleDescriptor, self).export_to_xml(resource_fs)
+
+    # Present only to appease pylint.
+    @classmethod
+    def from_xml(cls, xml_data, system, id_generator):
+        return super(MakoModuleDescriptor, cls).from_xml(xml_data, system, id_generator)
